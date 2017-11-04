8939534316ade1155d6d6725a09eb89cb710253b
ronak5279 <ronak5279.agarwal@gmail.com>
2017-11-04 11:50:11 -0700

    3X3 Same Mobile Dual Player Built

--- app/src/main/java/com/example/img_144/sutictac_as/SMDP3X3.java
+++ app/src/main/java/com/example/img_144/sutictac_as/SMDP3X3.java
@@ -1,21 +1,30 @@
 package com.example.img_144.sutictac_as;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SMDP3X3 extends AppCompatActivity {
     Button b[] = new Button[9];
     Boolean turn;
     int i;
-    int win[][]={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    Integer[] my = {0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 3, 6, 1, 4, 7, 2, 5, 8, 0, 4, 8, 2, 4, 6};
+    List<Integer> win = Arrays.asList(my);
     List<Integer> myuser = new ArrayList<Integer>();
     List<Integer> opponent = new ArrayList<Integer>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,28 +42,77 @@ public class SMDP3X3 extends AppCompatActivity {
         b[7] = (Button) findViewById(R.id.b7);
         b[8] = (Button) findViewById(R.id.b8);
 
-        View.OnClickListener buttonListener = new View.OnClickListener() {
+        for (i = 0; i < 9; i++) {
+            setOnClick(b[i], i);
+        }
+    }
+
+    private void setOnClick(final Button btn, final Integer x) {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Button b = (Button) v;
                 if (b.getText().toString().equals("")) {
                     if (turn) {
-                        myuser.add(b.getId());
-                        Log.d("me","P1"+myuser);
+                        myuser.add(x);
                         turn = false;
                         b.setText("X");
+                        endGame(myuser);
                     } else {
-                        opponent.add(b.getId());
-                        Log.d("me","P2"+opponent);
+                        opponent.add(x);
                         turn = true;
                         b.setText("O");
+                        endGame(opponent);
                     }
                 }
             }
-        };
+        });
+    }
 
-        for(i=0;i<9;i++) {
-            b[i].setOnClickListener(buttonListener);
+    private void endGame(List a1) {
+        List<Set<Integer>> x = getSubsets(a1, 3);
+        if (x.size() > 0) {
+            for (int i = 0; i < x.size(); i++) {
+                for (int j = 0; j < 8; j++) {
+                    Set<Integer> abs = new HashSet<Integer>();
+                    abs.addAll(Arrays.asList(new Integer[]{win.get(3 * j), win.get(3 * j + 1), win.get(3 * j + 2)}));
+                    if (abs.equals(x.get(i))) {
+                        Intent main = new Intent(this, MainActivity.class);
+                        startActivity(main);
+                    }
+                }
+            }
         }
+    }
+
+    private static void getSubsets(List<Integer> superSet, int k, int idx, Set<Integer> current, List<Set<Integer>> solution) {
+        //successful stop clause
+        if (current.size() == k) {
+            solution.add(new HashSet<>(current));
+            return;
+        }
+        //unseccessful stop clause
+        if (idx == superSet.size()) return;
+        Integer x = superSet.get(idx);
+        current.add(x);
+        //"guess" x is in the subset
+        getSubsets(superSet, k, idx + 1, current, solution);
+        current.remove(x);
+        //"guess" x is not in the subset
+        getSubsets(superSet, k, idx + 1, current, solution);
+    }
+
+    public static List<Set<Integer>> getSubsets(List<Integer> superSet, int k) {
+        List<Set<Integer>> res = new ArrayList<>();
+        getSubsets(superSet, k, 0, new HashSet<Integer>(), res);
+        return res;
+    }
+
+    public <T> List<T> twoDArrayToList(T[][] twoDArray) {
+        List<T> list = new ArrayList<T>();
+        for (T[] array : twoDArray) {
+            list.addAll(Arrays.asList(array));
+        }
+        return list;
     }
 }
