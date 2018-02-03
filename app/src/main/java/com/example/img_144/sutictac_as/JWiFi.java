b4d9bdf460a183d01709fa277d77076cab0017bf
ronak5279 <ronak5279.agarwal@gmail.com>
2018-02-03 14:10:25 +0530

    peer list showing

--- app/src/main/java/com/example/img_144/sutictac_as/JWiFi.java
+++ app/src/main/java/com/example/img_144/sutictac_as/JWiFi.java
@@ -1,163 +1,100 @@
 package com.example.img_144.sutictac_as;
 
-import android.annotation.SuppressLint;
-import android.app.Dialog;
+
 import android.content.BroadcastReceiver;
-import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.net.wifi.WifiManager;
 import android.content.Context;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-import java.util.List;
-
-import static android.R.id.list;
-
 public class JWiFi extends AppCompatActivity {
-    WifiManager mainWifiObj;
-    //WifiScanReceiver wifiReciever;
-    ListView list;
-    String wifis[];
+    public TextView mTextView;
+    public ListView mListView;
+    private Button searchBtn, playBtn;
+    private IntentFilter mIntentFilter;
+    private ArrayAdapter<String> wifiP2pArrayAdapter;
 
-    EditText pass;
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private BroadcastReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jwi_fi);
-/*
-        TextView config=(TextView) findViewById(R.id.text);
 
-        WifiManager onwifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        onwifi.setWifiEnabled(true);
-        wificonfig(config,onwifi);
-    }
-    protected void wificonfig(TextView config,WifiManager onwifi) {
-        int totalTime = 50000; // in nanoseconds
-        long startTime = System.nanoTime();
-        boolean toFinish = false;
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
-        while (!toFinish && !onwifi.isWifiEnabled())
-        {
-            toFinish = (System.nanoTime() - startTime >= totalTime);
-        }
-        onwifi.startScan();
-*/
-        ListView list = (ListView) findViewById(R.id.list);
-        mainWifiObj = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        mainWifiObj.setWifiEnabled(true);
-        int tTime = 5000000; // in nanoseconds
-        long sTime = System.nanoTime();
-        boolean toFin = false;
+        mTextView = (TextView) findViewById(R.id.textView2);
+        mListView = (ListView) findViewById(R.id.list);
 
-        while (!toFin && !mainWifiObj.isWifiEnabled()) {
-            toFin = (System.nanoTime() - sTime >= tTime);
-        }
-        int totalTime = 2000000000; // in nanoseconds
-        long startTime = System.nanoTime();
-        boolean toFinish = false;
+        wifiP2pArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        mListView.setAdapter(wifiP2pArrayAdapter);
 
-        while (!toFinish) {
-            toFinish = (System.nanoTime() - startTime >= totalTime);
-            //wifiReciever = new WifiScanReceiver();
-            mainWifiObj.startScan();
-            // listening to single list item on click
-            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    // selected item
-                    String ssid = ((TextView) view).getText().toString();
-                    connectToWifi(ssid);
-                    Toast.makeText(JWiFi.this, "Wifi SSID : " + ssid, Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
-
-/*
-    protected void onPause() {
-        unregisterReceiver(wifiReciever);
-        super.onPause();
-    }
-
-    protected void onResume() {
-        registerReceiver(wifiReciever, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        super.onResume();
-    }
-*/
-    class WifiScanReceiver extends BroadcastReceiver {
-        @SuppressLint("UseValueOf")
-        public void onReceive(Context c, Intent intent) {
-            List<ScanResult> wifiScanList = mainWifiObj.getScanResults();
-            wifis = new String[wifiScanList.size()];
-            for(int i = 0; i < wifiScanList.size(); i++){
-                wifis[i] = ((wifiScanList.get(i)).toString());
-            }
-            String filtered[] = new String[wifiScanList.size()];
-            int counter = 0;
-            for (String eachWifi : wifis) {
-                String[] temp = eachWifi.split(",");
-
-                filtered[counter] = temp[0].substring(5).trim();//+"\n" + temp[2].substring(12).trim()+"\n" +temp[3].substring(6).trim();//0->SSID, 2->Key Management 3-> Strength
-
-                counter++;
-
-            }
-            list.setAdapter(new ArrayAdapter<String>(getApplicationContext(),R.layout.lwi_fi,R.id.label, filtered));
-
-        }
-    }
-
-    private void finallyConnect(String networkPass, String networkSSID) {
-        WifiConfiguration wifiConfig = new WifiConfiguration();
-        wifiConfig.SSID = String.format("\"%s\"", networkSSID);
-        wifiConfig.preSharedKey = String.format("\"%s\"", networkPass);
-
-        // remember id
-        int netId = mainWifiObj.addNetwork(wifiConfig);
-        mainWifiObj.disconnect();
-        mainWifiObj.enableNetwork(netId, true);
-        mainWifiObj.reconnect();
-
-        WifiConfiguration conf = new WifiConfiguration();
-        conf.SSID = "\"\"" + networkSSID + "\"\"";
-        conf.preSharedKey = "\"" + networkPass + "\"";
-        mainWifiObj.addNetwork(conf);
-    }
-
-    private void connectToWifi(final String wifiSSID) {
-        final Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.connect);
-        dialog.setTitle("Connect to Network");
-        TextView textSSID = (TextView) dialog.findViewById(R.id.textSSID1);
-
-        Button dialogButton = (Button) dialog.findViewById(R.id.okButton);
-        pass = (EditText) dialog.findViewById(R.id.textPassword);
-        textSSID.setText(wifiSSID);
-
-        // if button is clicked, connect to the network;
-        dialogButton.setOnClickListener(new View.OnClickListener() {
+        searchBtn = (Button) findViewById(R.id.search_button);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String checkPassword = pass.getText().toString();
-                finallyConnect(checkPassword, wifiSSID);
-                dialog.dismiss();
+                search(v);
             }
         });
-        dialog.show();
+
+        playBtn = (Button) findViewById(R.id.play_button);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play(v);
+            }
+        });
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+    }
+    public void search(View view) {
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                mTextView.setText("Wifi-Direct Searching...");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                mTextView.setText("Error: Code " + reason);
+            }
+        });
+    }
+    public void play(View view) {
+        Toast.makeText(getApplicationContext(), "The game has been connected", Toast.LENGTH_LONG).show();
+    }
+    public void displayPeers(WifiP2pDeviceList peerlist) {
+        wifiP2pArrayAdapter.clear();
+        for(WifiP2pDevice peer : peerlist.getDeviceList()) {
+            wifiP2pArrayAdapter.add(peer.deviceName + "\n" + peer.deviceAddress);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
     }
 }
--- app/src/main/java/com/example/img_144/sutictac_as/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/example/img_144/sutictac_as/WiFiDirectBroadcastReceiver.java
@@ -2,21 +2,61 @@ package com.example.img_144.sutictac_as;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.IntentFilter;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
-public class WiFiDirectBroadcastReceiver extends AppCompatActivity {
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
-    IntentFilter mIntentFilter;
+
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private JWiFi mJwiFi;
+    private List<WifiP2pDevice> mPeers;
+    private List<WifiP2pConfig> mConfigs;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, JWiFi jwifi) {
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mJwiFi = jwifi;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        mIntentFilter = new IntentFilter();
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                mJwiFi.mTextView.setText("Wifi-Detect: Enabled");
+            } else {
+                mJwiFi.mTextView.setText("Wifi-Detect: Disabled");
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            mPeers = new ArrayList<WifiP2pDevice>();
+            mConfigs = new ArrayList<WifiP2pConfig>();
+
+            if (mManager != null) {
+                WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+                    @Override
+                    public void onPeersAvailable(WifiP2pDeviceList peerList) {
+                        mPeers.clear();
+                        mPeers.addAll(peerList.getDeviceList());
+                        mJwiFi.displayPeers(peerList);
+                        mPeers.addAll(peerList.getDeviceList());
+                        for(int i=0; i<peerList.getDeviceList().size(); i++) {
+                            WifiP2pConfig config = new WifiP2pConfig();
+                            config.deviceAddress = mPeers.get(i).deviceAddress;
+                            mConfigs.add(config);
+                        }
+                    }
+                };
+                mManager.requestPeers(mChannel, peerListListener);
+            }
+        }
     }
 }
